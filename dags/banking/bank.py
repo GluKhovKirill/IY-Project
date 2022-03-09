9cc43df500088d3c8b49e8f1f0f53f145ab5423f
Gluhov Kirill <kirill.gluhov2003@yandex.ru>
2022-03-09 15:08:21 +0300

    [feature] Переписан способ хранения счетов

--- dags/banking/bank.py
+++ dags/banking/bank.py
@@ -8,6 +8,8 @@ class VariablePlug:
     def __init__(self):
         self.variables = {
             'transaction_commission': 0.02,
+            'incoming_transactions': [], # UIDFrom, UIDTo, Amount
+            'outgoing_transactions': [],
         }
 
     def get(self, key, default_var=None, deserialize_json=False):
@@ -21,7 +23,8 @@ class VariablePlug:
     pass
 
 
-Variable = VariablePlug()
+#Variable = VariablePlug()
+from airflow.models import Variable
 # ----------------------------------------------------------------------------------------------------------------
 
 
@@ -30,35 +33,50 @@ class BankAccountError(BankError): pass
 
 
 class Account:
-    def __init__(self, uid: int, balance: float, is_active: bool) -> None:
+    def __init__(self, uid: int, is_active: bool) -> None:
         """
         :param int uid: Unique ID счета
-        :param float balance: Остаток на счете
         :param bool is_active:
         """
         # TODO: Add UID validation
         if not isinstance(uid, int):
             raise TypeError("UID must be integer")
-        if not (isinstance(balance, int) or isinstance(balance, float)):
-            raise TypeError("Balance must be integer or float")
         if not isinstance(is_active, bool):
             raise TypeError("IsActive must be integer")
-        self.__uid, self.__balance, self.__is_active = uid, balance, is_active
+        self.__uid, self.__is_active = uid, is_active
 
     def get_uid(self) -> int:
         return self.__uid
 
     def get_balance(self) -> float:
-        return self.__balance
+        incoming = Variable.get('incoming_transactions', default_var=[])
+        outgoing = Variable.get('outgoing_transactions', default_var=[])
 
-    def set_balance(self, balance: float) -> None:
+        incoming_filtered = filter(lambda x: x[1] == self.__uid, incoming)
+        outgoing_filtered = filter(lambda x: x[1] == self.__uid, outgoing)
+        balance = sum(map(lambda x: x[2], incoming_filtered)) - sum(map(lambda x: x[2], outgoing_filtered))
+        return balance
+
+    def commit_transaction(self, from_account_uid, balance: float) -> None: #TODO:
         if not self.__is_active:
             raise BankAccountError("Account isn't active")
         if not (isinstance(balance, float) or isinstance(balance, int)):
             raise BankAccountError("Balance must be integer or float")
-        if balance < 0:
-            raise BankAccountError("Balance must be at least 0")
-        self.__balance = balance
+        if not balance:
+            return
+        transaction = (from_account_uid, self.get_uid(), abs(balance))
+
+        incoming = Variable.get('incoming_transactions', default_var=[])
+        outgoing = Variable.get('outgoing_transactions', default_var=[])
+
+        if balance > 0:
+            incoming.append(transaction)
+            Variable.set('incoming_transactions', incoming)
+        else:
+            outgoing.append(transaction)
+            Variable.set('outgoing_transactions', outgoing)
+
+
     pass
 
 
@@ -67,20 +85,22 @@ class Bank:
     def get_commission() -> float:
         return Variable.get('transaction_commission', 0)
 
+    @staticmethod
+    def load_account(uid):
+        return Account(uid=uid, is_active=True)
+
     def __init__(self) -> None:
         self.bank_uid = Variable.get('bank_uid', default_var=1)
-        self.accounts = [Account(uid=self.bank_uid,
-                                 balance=5 * (10 ** 6),
-                                 is_active=True)]
-
-    def create_account(self) -> int:
-        uid = len(self.accounts)+1
+        self.accounts = [self.load_account(self.bank_uid)]
+        self.accounts[0].commit_transaction(0,1000)
+    '''
+    def _create_account(self, i) -> int:
+        uid = i
         new_account = Account(uid=uid,
-                              balance=0,
                               is_active=True)
         self.accounts.append(new_account)
         return uid
-
+    '''
     def get_account(self, uid: int) -> Account:
         """
         :param int uid:
@@ -96,6 +116,7 @@ class Bank:
     def get_bank_account(self) -> Account:
         return self.get_account(self.bank_uid)
 
+    '''
     def close_account(self, uid: int) -> tuple[bool, str]:
         if not isinstance(uid, int): raise TypeError("UID must be integer")
         try:
@@ -106,6 +127,7 @@ class Bank:
             return True, ""
         except BankError as err:
             return False, str(err)
+    '''
     pass
 
 
@@ -135,13 +157,13 @@ class BankHandler:
             fee = bank.get_commission() * amount
             bank_account = bank.get_bank_account()
 
-        new_value_a = self.account_a.get_balance() - amount
-        new_value_b = self.account_b.get_balance() + (amount-fee)
+        new_value_a = -amount
+        new_value_b = (amount-fee)
 
-        self.account_a.set_balance(new_value_a)
-        self.account_b.set_balance(new_value_b)
+        self.account_a.commit_transaction(self.account_b.get_uid(), new_value_a)
+        self.account_b.commit_transaction(self.account_a.get_uid(), new_value_b)
         if with_fee:
-            bank_account.set_balance(bank_account.get_balance() + fee)
+            bank_account.commit_transaction(self.account_a.get_uid(), fee)
     pass
 
 
@@ -149,21 +171,26 @@ def main():
     bank = Bank()
     Variable.set('bank', bank)  # TODO: make sure that it'll take a ride
 
-    a_account_uid = bank.create_account()
+    a_account_uid = bank._create_account()
     a_account = bank.get_account(a_account_uid)
     handler = BankHandler(bank.get_bank_account(), a_account)
     handler.transfer(100, with_fee=False)
 
-    b_account_uid = bank.create_account()
+    b_account_uid = bank._create_account()
     b_account = bank.get_account(b_account_uid)
     handler = BankHandler(bank.get_bank_account(), b_account)
     handler.transfer(100, with_fee=False)
+    print(a_account.get_balance(), b_account.get_balance(), bank.get_bank_account().get_balance())
 
     handler = BankHandler(a_account, b_account)
     handler.transfer(50)
 
     print(a_account.get_balance(), b_account.get_balance(), bank.get_bank_account().get_balance())
     print(bank.get_account(1))
+
+    incoming = Variable.get('incoming_transactions', default_var=[])
+    outgoing = Variable.get('outgoing_transactions', default_var=[])
+    print(incoming, outgoing)
     pass
 
 
