2e013d39a7eae2d1738f6affd2b6dfc0c5844ea2
Glukhov Kirill <kirill.gluhov2003@yandex.ru>
2022-05-11 13:52:07 +0300

    Update 25_banking_transaction.py

--- dags/25_banking_transaction.py
+++ dags/25_banking_transaction.py
@@ -1,6 +1,6 @@
 import time
 from datetime import datetime
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, get_current_context
 from airflow.models import Variable
 from airflow import *
 import json
@@ -20,16 +20,43 @@ with DAG(
     tags=['dev'],
     default_args=args,
 ) as dag:
-    def pull_cmd(**query):
+    def pull_cmd(**query, ):
+        #context = get_current_context()
+        #context['dag_run'].conf('acc_from')
+        #print(dag)
         # query = Variable.get(key="IU25TransactionQuery", deserialize_json=True)
         print("kwargs", query)
-        #request = f"""INSERT INTO city."bankAccount" (acc_from, acc_to, amount) VALUES
-        #    ({query['from_acc']}, {query['to_acc']}, {query['amount']});"""
-        request = ''
+        request = """INSERT INTO city."bankAccount" (acc_from, acc_to, amount) VALUES
+            ({{ ti.xcom_pull(\'DataBaseInsertHelper\')['from_acc'] }}, {{ti.xcom_pull(\'DataBaseInsertHelper\')['to_acc']}}, {{ti.xcom_pull(\'DataBaseInsertHelper\')['amount']}});"""
+        #query['ti'].xcom_pull('DataBaseInsertHelper')
+        #request = 'select 1'
         return request
+
+    def helper(**context):
+        print(context['dag_run'].conf)
+
+        return context['dag_run'].conf
+
+    def helpertest(**context):
+        print(context['ti'].xcom_pull('DataBaseInsertHelper'))
+
+        return context['dag_run'].conf
 
     run_this_after = PostgresOperator(
         task_id="DataBaseInsert",
         sql=pull_cmd(),
         postgres_conn_id="iu25",
     )
+
+    operator2 = PythonOperator(
+        provide_context=True,
+        task_id="DataBaseInsertHelper",
+        python_callable=helper
+    )
+
+    operator3 = PythonOperator(
+        provide_context=True,
+        task_id="test",
+        python_callable=helpertest
+    )
+    operator2 >>operator3>> run_this_after
